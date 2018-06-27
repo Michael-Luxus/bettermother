17556dea87f8a4dff16eb10b82ac0184a5567ece
Michael-Luxus <michaelluxusnew@gmail.com>
2018-06-27 17:03:04 +0300

    better

---
+++ app.js
@@ -0,0 +1,8 @@
+function maFonction(){
+    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
+
+function changerFond(){
+    var element = document.getElementById("page");
+    element.style.backgroundColor ="magenta";
+}
