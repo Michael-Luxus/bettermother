cd42239d9074246cf25c7052bc4ba45d5a442cfd
Michael-Luxus <michaelluxusnew@gmail.com>
2018-06-28 11:39:44 +0300

    better

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 function maFonction(){
-    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+   var w = window.open('','','width=450,height=100,left=450,top=250',)
+w.document.write('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')
+w.focus()
+setTimeout(function() {w.close();}, 10000)
 }
-
 function changerFond(){
     var element = document.getElementById("page");
     element.style.backgroundColor ="magenta";
+
+    element.style.color ="blue";
 }
